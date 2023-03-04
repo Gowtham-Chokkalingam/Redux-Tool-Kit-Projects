ae1cf1af04bde2306e840f9a5276a75e40ad160f
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-03-04 13:47:14 +0530

    Added Cart item ui

--- RTK-Cart-Project/src/Components/CartItem.jsx
+++ RTK-Cart-Project/src/Components/CartItem.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
 
-const CartItem = () => {
+const CartItem = ({ amount, price, title, img, id }) => {
   return (
-    <div>CartItem</div>
-  )
-}
+    <article className="cart-item">
+      <img src={img} alt={title}></img>
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">Remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp></ChevronUp>
+        </button>
 
-export default CartItem
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown></ChevronDown>
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
