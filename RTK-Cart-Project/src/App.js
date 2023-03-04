5b3a50463a59238a203e13d0f3a87189a2fa187a
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-03-04 14:48:55 +0530

    Added Cart Increament and Decrement functionality

--- RTK-Cart-Project/src/App.js
+++ RTK-Cart-Project/src/App.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./Components/CartContainer";
 import Navbar from "./Components/Navbar";
+import { calculateTotals } from "./Features/cart/cartSlice";
 
 function App() {
-  return<main>
-    <Navbar></Navbar>
-    <CartContainer></CartContainer>
-  </main>;
+  const dispatch = useDispatch();
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      <Navbar></Navbar>
+      <CartContainer></CartContainer>
+    </main>
+  );
 }
 export default App;
--- RTK-Cart-Project/src/cartItems.js
+++ RTK-Cart-Project/src/cartItems.js
@@ -29,4 +29,6 @@ const cartItems = [
   },
 ];
 
+
+
 export default cartItems;
